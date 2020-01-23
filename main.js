e6830c1adfe0492af4cf2bd5e2122031f1639d6b
AlbertGabrielyan1991 <albertgabrielyan1991@gmail.ru>
2020-01-23 20:08:32 +0400

    first commit by master

---
+++ main.js
@@ -0,0 +1,13 @@
+// fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data?frequency=1d&filter=history&period1=1546448400&period2=1562086800&symbol=AMRN", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+//         "x-rapidapi-key": "d267386d7dmshc08ec9bfcebc6b6p14c77cjsn49b07a22fe74"
+//     }
+// })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
